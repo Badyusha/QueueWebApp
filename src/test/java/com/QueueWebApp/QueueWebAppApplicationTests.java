b6ed2f953f23dc3ad09ae947719fdcd9e57a932b
Badyusha <pashab200409@gmail.com>
2024-01-30 13:51:45 +0300

    first commit

---
+++ src/test/java/com/QueueWebApp/QueueWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.QueueWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueueWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

ab94ee39c4ac91d59e0e9812a22384cbea30b37c
Badyusha <pashab200409@gmail.com>
2024-02-04 23:39:57 +0300

    removed all undone controller, services, .jsp

--- src/main/java/com/QueueWebApp/bll/services/ProfileService.java
+++
@@ -1,6 +0,0 @@
-package com.QueueWebApp.bll.services;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class ProfileService { }
--- src/main/java/com/QueueWebApp/bll/services/QueueService.java
+++
@@ -1,7 +0,0 @@
-package com.QueueWebApp.bll.services;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class QueueService {
-}
